d5761a8b564b3667d81574c4fd37cec36c3a0f1e
Ricardo Júnio Ferreira <rjunio@iesb.br>
2020-02-11 18:06:10 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.css";
+
+const Header = () => (<header id="main-header">JsHunt</header>);
+
+export default Header;
+
+
+// class Header extends Component {
+//     render(){
+//         return <h1>Hello Wolrd</h1>
+//     }
+// }
